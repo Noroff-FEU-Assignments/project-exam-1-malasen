c1c69c5b69486a69e78fbaf0b6c6f5b5aa0f74aa
malasen <71284217+malasen@users.noreply.github.com>
2021-05-09 19:17:40 +0200

    some fixes

--- js/blog.js
+++ js/blog.js
@@ -6,16 +6,17 @@ async function getPost(){
     try {
         const response = await fetch(postsUrl);
         const results = await response.json();
-        console.log(results);
+        //console.log(results);
 
         for(let i= 0; i < results.length; i++){
-            console.log(results[i].title);
+            //console.log(results[i].title);
             loader.classList.remove("loader");
-            postsContainer.innerHTML += `<div class="card" style="background-image:url('${results[i]._embedded["wp:featuredmedia"][0].source_url}')">
+            postsContainer.innerHTML += `<div class="card">
+                                         <a href="blogpost.html?id=${results[i].id}">
                                          <h3>${results[i].title.rendered}</h3>
                                          <div>${results[i].excerpt.rendered}</div>
-                                         <a href="blogpost.html?id=${results[i].id}"> Continue to read... </a>s
                                          <time>Posted: ${results[i].date} </time> 
+                                         </a>
                                          </div>`
         }
     } catch {
