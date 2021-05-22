ab90dd3dfa5961e129c0de592b6a73a27070a7cb
malasen <71284217+malasen@users.noreply.github.com>
2021-05-22 21:26:53 +0200

    modal

--- js/blog-details.js
+++ js/blog-details.js
@@ -1,6 +1,10 @@
 const querystring = document.location.search;
 const params = new URLSearchParams(querystring);
 const id = params.get("id");
+const titleUpdate = document.querySelector("title");
+const modalImg = document.querySelector(".modal img")
+
+
 
 const postsUrl = "https://muel.no/productapi/wp-json/wp/v2/posts/" +id + "?_embed";
 const postsContainer = document.querySelector(".postcontainer");
@@ -13,7 +17,11 @@ async function getPost(){
 
         loader.style.display="none";
         postsContainer.innerHTML +="<h2>"+ results.title.rendered + "</h2>"+ results.content.rendered;
-        
+        titleUpdate.innerHTML = "Blog | " + results.title.rendered;
+
+        const img = document.querySelector(".postcontainer img");
+        modalImg.innerHTML = img;
+
     } catch {
         console.log (error);
     }
